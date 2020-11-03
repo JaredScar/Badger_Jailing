a258e1cbd2926273a2235e963d2fe91355d95fc8
Jared <TheWolfBadger@gmail.com>
2020-11-03 10:39:22 -0500

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,64 @@
+jailTime = nil;
+cords = nil;
+jailCell = nil;
+RegisterNetEvent('Badger_Jailing:JailPlayer')
+AddEventHandler('Badger_Jailing:JailPlayer', function(jailCoords, time, cell)
+	local ped = GetPlayerPed(-1);
+	jailTime = time;
+	cords = jailCoords;
+	jailCell = cell;
+	SetEntityCoords(ped, cords.x, cords.y, cords.z, 1, 0, 0, 1)
+end)
+
+RegisterNetEvent('Badger_Jailing:UnjailPlayer')
+AddEventHandler('Badger_Jailing:UnjailPlayer', function()
+	jailTime = nil;
+	local coords = Config.PrisonExit;
+	local ped = GetPlayerPed(-1);
+	SetEntityCoords(ped, coords.x, coords.y, coords.z, 1, 0, 0, 1)
+	TriggerEvent('chatMessage', Config.Prefix .. "You have been released from jail!...");
+	TriggerServerEvent('Badger_Jailing:FreeCell', jailCell);
+	jailCell = nil;
+	cords = nil;
+end)
+
+Citizen.CreateThread(function()
+	TriggerServerEvent("Badger_Jailing:Connected");
+	while true do 
+		Citizen.Wait(1000);
+		local ped = GetPlayerPed(-1)
+		if jailTime ~= nil then 
+			if jailTime > 0 then 
+				jailTime = jailTime - 1;
+			end
+			if mod(jailTime, 5) == 0 and jailTime ~= 0 then 
+				TriggerEvent('chatMessage', Config.Prefix .. "You have ^1" .. jailTime .. "^3 seconds left in jail...");
+				SetEntityCoords(ped, cords.x, cords.y, cords.z, 1, 0, 0, 1)
+			end
+			if jailTime == 0 then 
+				TriggerEvent('Badger_Jailing:UnjailPlayer');
+				jailTime = nil;
+			end
+		end
+	end
+end)
+function mod(a, b)
+    return a - (math.floor(a/b)*b)
+end
+function Draw2DText(x, y, text, scale, center)
+    -- Draw text on screen
+    SetTextFont(4)
+    SetTextProportional(7)
+    SetTextScale(scale, scale)
+    SetTextColour(255, 255, 255, 255)
+    SetTextDropShadow(0, 0, 0, 0,255)
+    SetTextDropShadow()
+    SetTextEdge(4, 0, 0, 0, 255)
+    SetTextOutline()
+    if center then 
+    	SetTextJustification(0)
+    end
+    SetTextEntry("STRING")
+    AddTextComponentString(text)
+    DrawText(x, y)
+end
---
+++ server.lua
@@ -0,0 +1,196 @@
+function SaveFile(data)
+	SaveResourceFile(GetCurrentResourceName(), "players.json", json.encode(data, { indent = true }), -1)
+end
+function LoadFile()
+	local al = LoadResourceFile(GetCurrentResourceName(), "players.json")
+    local cfg = json.decode(al)
+    return cfg;
+end
+function ExtractIdentifiers(src)
+    local identifiers = {
+        steam = "",
+        ip = "",
+        discord = "",
+        license = "",
+        xbl = "",
+        live = ""
+    }
+
+    --Loop over all identifiers
+    for i = 0, GetNumPlayerIdentifiers(src) - 1 do
+        local id = GetPlayerIdentifier(src, i)
+
+        --Convert it to a nice table.
+        if string.find(id, "steam") then
+            identifiers.steam = id
+        elseif string.find(id, "ip") then
+            identifiers.ip = id
+        elseif string.find(id, "discord") then
+            identifiers.discord = id
+        elseif string.find(id, "license") then
+            identifiers.license = id
+        elseif string.find(id, "xbl") then
+            identifiers.xbl = id
+        elseif string.find(id, "live") then
+            identifiers.live = id
+        end
+    end
+
+    return identifiers
+end
+JailTracker = {};
+RegisterCommand('jail', function(src, args, raw)
+	-- /jail <id> <time> 
+	if IsPlayerAceAllowed(src, "Badger_Jailing.Jail") then 
+		if #args < 2 then 
+			-- Not enough args 
+			TriggerClientEvent('chatMessage', src, Config.Prefix .. "^1ERROR: Invalid usage. ^2Usage: /jail <id> <time>");
+			return;
+		end
+		if GetPlayerIdentifiers(args[1])[1] ~= nil then 
+			-- Valid player 
+			if tonumber(args[2]) ~= nil then 
+				-- Valid number supplied 
+				TriggerClientEvent('chatMessage', -1, Config.Prefix .. "Player ^5" .. GetPlayerName(args[1]) .. " ^3has been jailed for ^1" ..
+					args[2] .. " ^3seconds...");
+				Citizen.CreateThread(function()
+					local cfg = LoadFile();
+					local ids = ExtractIdentifiers(args[1]);
+					cfg[ids.license] = {Cell = nil, Time = tonumber(args[2])};
+					SaveFile(cfg); 
+					while not IsCellFree() do
+						TriggerClientEvent('chatMessage', args[1], Config.Prefix .. "Waiting on a free cell at jail..."); 
+						Citizen.Wait(10000);
+					end
+					local key = GetFreeCell();
+					local coords = Config.Cells[key];
+					CellTracker[key] = ids.license;
+					local cfg = LoadFile();
+					cfg[ids.license] = {Cell = key, Time = tonumber(args[2])};
+					JailTracker[tonumber(args[1])] = tonumber(args[2]);
+					SaveFile(cfg); 
+					TriggerClientEvent('Badger_Jailing:JailPlayer', tonumber(args[1]), coords, tonumber(args[2]), key);
+				end)
+			else 
+				-- Invalid number supplied 
+				TriggerClientEvent('chatMessage', src, Config.Prefix .. "^1ERROR: The 2nd argument was not a proper number...");
+			end
+		else 
+			-- Invalid player 
+			TriggerClientEvent('chatMessage', src, Config.Prefix .. "^1ERROR: Invalid player supplied...");
+		end
+	end
+end)
+
+RegisterCommand('free', function(src, args, raw)
+	-- /unjail <id> 
+	if IsPlayerAceAllowed(src, "Badger_Jailing.Unjail") then 
+		if #args ~= 1 then 
+			-- Not enough args 
+			TriggerClientEvent('chatMessage', src, Config.Prefix .. "^1ERROR: Invalid usage. ^2Usage: /free <id>");
+			return;
+		end
+		if GetPlayerIdentifiers(args[1])[1] ~= nil then 
+			-- Valid player 
+			TriggerClientEvent('chatMessage', -1, Config.Prefix .. "Player ^5" .. GetPlayerName(args[1]) .. " ^3has been released from jail by ^2" 
+				.. GetPlayerName(src));
+
+			TriggerClientEvent('Badger_Jailing:UnjailPlayer', args[1]);
+			local ids = ExtractIdentifiers(args[1]);
+			local cfg = LoadFile();
+			cfg[ids.license] = nil;
+			JailTracker[tonumber(args[1])] = nil;
+			SaveFile(cfg); 
+		else 
+			-- Not valid player 
+			TriggerClientEvent('chatMessage', src, Config.Prefix .. "^1ERROR: Invalid player supplied...");
+		end
+	end
+end)
+Citizen.CreateThread(function()
+	while true do 
+		Citizen.Wait(1000);
+		for k, v in pairs(JailTracker) do 
+			if JailTracker[k] ~= nil and JailTracker[k] > 0 then 
+				JailTracker[k] = JailTracker[k] - 1;
+			end
+			if JailTracker[k] ~= nil and JailTracker[k] == 0 then 
+				JailTracker[k] = nil;
+			end
+		end
+	end
+end)
+RegisterNetEvent("Badger_Jailing:Connected")
+AddEventHandler("Badger_Jailing:Connected", function()
+	local src = source;
+	local ids = ExtractIdentifiers(src);
+	local cfg = LoadFile();
+	if cfg[ids.license] ~= nil then 
+		local time = cfg[ids.license].Time
+		local cell = cfg[ids.license].Cell;
+		if CellTracker[cell] == nil then 
+			-- Jail them in this cell 
+			CellTracker[cell] = ids.license;
+			local coords = Config.Cells[cell];
+			TriggerClientEvent('Badger_Jailing:JailPlayer', tonumber(src), coords, time, cell);
+		else 
+			-- Jail them in another cell 
+			Citizen.CreateThread(function()
+				while not IsCellFree() do
+					TriggerClientEvent('chatMessage', src, Config.Prefix .. "Waiting on a free cell at jail..."); 
+					Citizen.Wait(10000);
+				end
+				local key = GetFreeCell();
+				local coords = Config.Cells[key];
+				local ids = ExtractIdentifiers(src);
+				CellTracker[key] = ids.license;
+				local cfg = LoadFile();
+				cfg[ids.license] = {Cell = key, Time = tonumber(time)};
+				JailTracker[src] = tonumber(time);
+				SaveFile(cfg); 
+				TriggerClientEvent('Badger_Jailing:JailPlayer', tonumber(src), coords, tonumber(time), key);
+			end)
+		end
+	end
+end)
+AddEventHandler("playerDropped", function()
+	local src = source;
+	local ids = ExtractIdentifiers(src);
+	local cfg = LoadFile();
+	if cfg[ids.license] ~= nil then 
+		cfg[ids.license].Time = JailTracker[src];
+	end
+	SaveFile(cfg);
+	JailTracker[src] = nil;
+	SaveFile(cfg);
+end)
+
+CellTracker = {}
+
+RegisterNetEvent('Badger_Jailing:FreeCell')
+AddEventHandler('Badger_Jailing:FreeCell', function(cell)
+	local ids = ExtractIdentifiers(source);
+	local cfg = LoadFile();
+	cfg[ids.license] = nil;
+	JailTracker[source] = nil;
+	SaveFile(cfg); 
+	CellTracker[cell] = nil;
+end)
+
+function GetFreeCell()
+	for k, v in pairs(Config.Cells) do
+		if CellTracker[k] == nil then 
+			return k;
+		end
+	end
+	return nil;
+end
+
+function IsCellFree()
+	for k, v in pairs(Config.Cells) do
+		if CellTracker[k] == nil then 
+			return true;
+		end
+	end
+	return false;
+end
